a5dd765fc98b9818c6632c13129b0964554675bc
bleachzou2 <bleachzou2@163.com>
2016-04-11 13:33:04 +0800

    完成vtkHideCubeCallback的编写，该类针对vmtkImageVOISelector（这是vmtk的python类） 的 def HideCube(self,object, event):编写
    Signed-off-by: bleachzou2 <bleachzou2@163.com>

---
+++ vtkHideCubeCallback.h
@@ -0,0 +1,46 @@
+#ifndef VTK_HIDE_CUBE_CALLBACK_
+#define VTK_HIDE_CUBE_CALLBACK_
+#include "vtkCommand.h"
+#include "vtkObject.h"
+#include "vtkBoxWidget.h"
+#include "vtkActor.h"
+#include "vtkCubeSource.h"
+
+/*
+*在使用这个类的时候，一定要把cubeActor传进来
+*/
+class vtkHideCubeCallback:public vtkCommand
+{
+public:
+
+	static vtkHideCubeCallback* New()
+	{
+		return new vtkHideCubeCallback;
+	}
+
+	virtual void Execute(vtkObject *caller, unsigned long ev,
+                void *callData)
+	{
+		if(ev == vtkCommand::DisableEvent||ev == vtkCommand::StartInteractionEvent)
+		{
+			if(cubeActor != 0)
+			{
+				cubeActor->VisibilityOff();
+			}
+		}
+
+	}
+	void setObject(vtkActor* ca)
+	{
+		cubeActor = ca;
+	}
+protected:
+	vtkHideCubeCallback(){};
+private:
+	
+	vtkActor* cubeActor;
+
+
+
+};
+#endif
