8075ede6ef949822d6172a7d12eaffd877a35d87
bleachzou2 <bleachzou2@163.com>
2016-05-08 09:01:38 +0800

    添加MergetLevelSet方法，但是还没实现

--- vmtkLevelSetSegmentation.h
+++ vmtkLevelSetSegmentation.h
@@ -30,10 +30,12 @@ public:
 	
 	vmtkLevelSetSegmentation();
 	virtual~vmtkLevelSetSegmentation();
-	//levelSetInput要处理的图像数据，levelSetOutput处理完之后的图像数据，levelSetOutput传进来之前已经分配内存
+	
 	
 
-
+	/*
+	  以下的四个方法图像指针都不能为空
+	*/
     void LevelSetEvolutionGEODESIC(vtkImageData*FeatureImage,vtkImageData*LevelSetsInput,vtkImageData*LevelSetsOutput);
 
 	void LevelSetEvolutionCURVES(vtkImageData*FeatureImage,vtkImageData*LevelSetsInput,vtkImageData*LevelSetsOutput);
@@ -41,6 +43,12 @@ public:
 	void LevelSetEvolutionTHRESHOLD(vtkImageData*originalImage,vtkImageData*LevelSetsInput,vtkImageData*LevelSetsOutput,double UpperThreshold = unassinedThreshHold,double LowerThreshold = unassinedThreshHold);
 
 	void LevelSetEvolutionLAPLACIAN(vtkImageData*originalImage,vtkImageData*LevelSetsInput,vtkImageData*LevelSetsOutput);
+
+	/*
+	*合并图像的方法,合并之后的结果放在LevelSetInput1
+	*/
+	void MergeMergeLevelSet(vtkImageData*LevelSetInput1,vtkImageData*LevelSetInput2);
+
 	
 };
 #endif
