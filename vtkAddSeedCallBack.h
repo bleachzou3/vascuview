d9116c2da6c7248815fa50f8dfdc1d104541fc52
bleachzou2 <bleachzou2@163.com>
2016-05-12 16:36:42 +0800

    完成了vtkAddSeedCallBack回调函数类

--- vtkAddSeedCallBack.h
+++ vtkAddSeedCallBack.h
@@ -2,6 +2,9 @@
 #define VTK_ADD_SEED_CALL_BACK_
 #include <vtkCommand.h>
 #include <vtkImageData.h>
+#include <vtkImagePlaneWidget.h>
+#include <vtkPolyData.h>
+#include <vtkRenderer.h>
 class vtkAddSeedCallBack:public vtkCommand
 {
 public:
@@ -12,15 +15,54 @@ public:
 	virtual void Execute(vtkObject *caller, unsigned long ev,
                 void *callData)
 	{
-
+		double cursorData[4] = {0.0,0.0,0.0,0.0};
+		vtkImagePlaneWidget* obj = dynamic_cast<vtkImagePlaneWidget*>(caller);
+		obj->GetCursorData(cursorData);
+		double spacing[3];
+		double origin[3];
+		Image->GetSpacing(spacing);
+		Image->GetOrigin(origin);
+		double point[3]={0.0,0.0,0.0};
+		point[0] = cursorData[0]*spacing[0]+origin[0];
+		point[1] = cursorData[1]*spacing[1]+origin[1];
+		point[2] = cursorData[2]*spacing[2]+origin[2];
+		Seeds->GetPoints()->InsertNextPoint(point);
+		Seeds->Modified();
+		renderer->Render();
+		
+	}
+	void setImage(vtkImageData*localImage)
+	{
+		Image = localImage;
+	}
+	void setSeeds(vtkPolyData*localSeeds)
+	{
+		Seeds = localSeeds;
+	}
+	void setRenderer(vtkRenderer* localRender)
+	{
+		renderer = localRender;
 	}
 protected:
 	vtkAddSeedCallBack()
 	{
+		
 
 	}
 private:
+	/*
+	*需要调用者传入
+	*/
 	vtkImageData* Image;
+	/*
+	*需要调用者传入
+	*/
+	vtkPolyData* Seeds;
+
+	/*
+	*需要调用者传入
+	*/
+	vtkRenderer* renderer;
 };
 
 
